cab2aa251cbefe39f7f7c7fa993dde3393caf09d
Bryce Gruber <dev@brycegruber.com>
2024-03-02 14:45:16 -0600

    final colors

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,7 @@ html_static_path = ['_static']
 html_theme_options = {
     'nav_title': 'MultiGP Toolkit',
 
-    'color_primary': 'orange',
-    'color_accent': 'grey',
+    'color_primary': 'indigo',
 
     'repo_url': 'https://github.com/i-am-grub/MultiGP_Toolkit',
     'repo_name': 'MultiGP Toolkit',
